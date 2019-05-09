89d3c7a42ea2959da0dad3d8f166a525c35e6977
Nicole W <nicoleewhitee@gmail.com>
2019-05-08 18:12:08 -0700

    Updates

---
+++ precache-manifest.ca311d3c9dc50217d028212406a9048e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b59fc0b8479316db60cab6a299a6088",
+    "url": "/RoseGame/index.html"
+  },
+  {
+    "revision": "c8093cd5259214fecfea",
+    "url": "/RoseGame/static/css/main.de0fda85.chunk.css"
+  },
+  {
+    "revision": "2b5b43459bd67b730b0e",
+    "url": "/RoseGame/static/js/2.0ab779d2.chunk.js"
+  },
+  {
+    "revision": "c8093cd5259214fecfea",
+    "url": "/RoseGame/static/js/main.9f2a1f2e.chunk.js"
+  },
+  {
+    "revision": "486483720a5054134a9e",
+    "url": "/RoseGame/static/js/runtime~main.19369d86.js"
+  }
+]);
